b4ac5ad35b2d468e3b875e51297663e99058c188
Esin Belen <esinbelen@hotmail.com>
2023-09-27 15:26:43 +0300

    database design is changed

--- LearningManagementSystem.Web/Models/Assignment.cs
+++ LearningManagementSystem.Web/Models/Assignment.cs
@@ -7,11 +7,8 @@ public class Assignment
     public int Id { get; set; }
     public string Description { get; set; }
     public string ContentPath { get; set; }
-    [ForeignKey("UserId")]
-    public int StudentId { get; set; }
     public int CourseId { get; set; }
     public DateTime DueDate { get; set; }
-    public List<User>? Users { get; set; }
-    public List<Course>? Courses { get; set; }
+    public Course Course { get; set; }
     
 }
--- LearningManagementSystem.Web/Models/Material.cs
+++ LearningManagementSystem.Web/Models/Material.cs
@@ -5,6 +5,7 @@ public class Material
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string ContentPath { get; set; } 
-    public List<Course>? Courses { get; set; }
+    public string ContentPath { get; set; }
+    public int CourseId { get; set; }
+    public Course Course{ get; set; }
 }
